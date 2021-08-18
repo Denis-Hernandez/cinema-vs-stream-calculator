938883f61c4aac246f447b687dc6ec8fa1123d1f
Denis Hernandez <kandor95@gmail.com>
2021-08-18 12:27:48 -0600

    editando el JS para generar a travez de una lista las opciones de Streamming de la App

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//import modulo from './option_list';
 function compararPrecios(){
     var precios = preciosDeStreamSeleccionados();
     var opcionPrincipal = "...";
@@ -41,4 +42,42 @@ function preciosDeStreamSeleccionados(){
         }
     });
     return seleccionado;
+}
+
+function generateListOfServices(){
+    let services = [
+        {name:'Netflix',price:9},
+        {name:'Disney+',price:8},
+        {name:'Prime Video',price:10},
+        {name:'Paramount+',price:5},
+        {name:'HBO Max',price:15},
+        {name:'Apple TV',price:5},];
+    let div = document.querySelector("#list-options")
+    services.forEach((service, index)=>{
+        let option =optionList(index,service.name,service.price);
+        div.appendChild(option);
+    });
+}
+
+function optionList(index,name,price){
+    let label = document.createElement("label");
+    let input = document.createElement("input");
+    let span = document.createElement("span");
+    span.className = 'container__form--checkmark';
+    Object.assign(input,{
+        className: 'container__form--input-check',
+        oninput: 'compararPrecios()',
+        type: 'checkbox',
+        value: price,
+        id: `option_${index}`,
+    });
+    input.addEventListener('input', compararPrecios)
+    Object.assign(label,{
+        className:'container__form--check-container',
+        htmlFor:`option_${index}`,
+        innerHTML: name
+    });
+    label.appendChild(input);
+    label.appendChild(span);
+    return label;
 }
